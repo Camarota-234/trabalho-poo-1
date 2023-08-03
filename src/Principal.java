288b8471ae229d90d24a6d2b88c867a1bc2ec6b8
JOÃO GUILHERME CARVALHO CAMAROTA <joao.camarota@discente.ufg.br>
2023-08-03 14:23:44 -0300

    mensagem

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,3 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo!");
-    }
+public public static void main(String[] args) {
+
 }
