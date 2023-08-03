73f778f2b0cfcfb2e3bda6b8114821cce052809e
JOÃO GUILHERME CARVALHO CAMAROTA <joao.camarota@discente.ufg.br>
2023-08-03 14:34:45 -0300

    bem vindos

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,7 @@
-public static void main(String[] args) {
-    System.out.println("Bem vindo");
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Bem vindo");
+    }
+
 }
